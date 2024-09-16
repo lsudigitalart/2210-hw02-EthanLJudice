4ed840d16ae6c49dbbe7e6b39adb958333340d69
EthanLJudice <124456727+EthanLJudice@users.noreply.github.com>
2024-09-16 12:27:58 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,79 @@
+//Author @Ethan Judice
 function setup() {
-  createCanvas(500, 500);
-  fill(0);
+  createCanvas(400, 269);
+  fill('black');
   ellipse(250, 250, 1000, 1000);
-  fill(255);
-  ellipse(0, 0, 207, 207);
-  ellipse(500, 0, 207, 207);
-  ellipse(0, 500, 207, 207);
-  ellipse(500, 500, 207, 207);
-  fill(0);
-  ellipse(0, 0, 150, 200);
-  ellipse(500, 0, 200, 150);
-  ellipse(0, 500, 200, 150);
-  ellipse(500, 500, 150, 200);
-  fill(255);
-  ellipse(250, 250, 500, 500);
-  fill(0);
-  ellipse(250, 250, 300, 50);
-  ellipse(-50, 250, 300, 150);
-  ellipse(550, 250, 300, 150);
-  ellipse(250, 250, 50, 300);
-  ellipse(250, 550, 150, 300);
-  ellipse(250, -50, 150, 300);
-  fill(255);
-  ellipse(250, 250, 70, 70);
-  fill(0);
-  ellipse(250, 250, 50, 50);
-  fill(255);
-  ellipse(-50, 250, 299, 100);
-  ellipse(550, 250, 299, 100);
-  ellipse(250, 550, 100, 299);
-  ellipse(250, -50, 100, 299);
-  fill(0);
-  ellipse(150, 150, 150, 150);
-  ellipse(350, 150, 150, 150);
-  ellipse(150, 350, 150, 150);
-  ellipse(350, 350, 150, 150);
-  fill(255);
-  ellipse(150, 150, 150, 50);
-  ellipse(350, 150, 50, 150);
-  ellipse(150, 350, 50, 150);
-  ellipse(350, 350, 150, 50);
+  //fill(255);
+
+
+
+
+
+
+
+  fill('yellow')
+  beginShape();
+  vertex(-1, -1);
+  vertex(-1, 135);
+  vertex(173, 130);
+  vertex(65, -1);
+  endShape(CLOSE)
+
+  fill('blue')
+  //Left Blue Square
+  beginShape();
+  vertex(301, 127);
+  vertex(170, 0);
+  vertex(75, 0);
+  vertex(180, 130);
+  endShape(CLOSE)
+  //Right Blue Square
+  beginShape();
+  vertex(380, -1);
+  vertex(245, -1);
+  vertex(316, 126);
+  vertex(401, 124);
+  vertex(401, 25);
+  endShape(CLOSE)
+
+  fill('white')
+  //Tiny White Corner Triangle
+  beginShape()
+  vertex(401, -1);
+  vertex(401, 10);
+  vertex(391, -1);
+  endShape(CLOSE)
+
+
+
+  fill('red')
+  //Bottom Red Line
+  beginShape();
+  vertex(401, 210);
+  vertex(0, 213);
+  vertex(0, 270);
+  vertex(401, 270);
+  endShape(CLOSE)
+  //Middle Red Line
+  beginShape();
+  vertex(401, 175);
+  vertex(0, 180);
+  vertex(0, 205);
+  vertex(401, 204);
+  endShape(CLOSE)
+  //Top Red Line
+  beginShape();
+  vertex(401, 130);
+  vertex(0, 140);
+  vertex(0, 174);
+  vertex(401, 168);
+  endShape(CLOSE)
+  //Top Red Triangle
+  beginShape();
+  vertex(235, -1);
+  vertex(310, 126);
+  vertex(180, -1);
+  endShape(CLOSE)
 }
 
 function draw() {
